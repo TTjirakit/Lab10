96dc9f4f3bf5d538286e7f526df13c915bd2532a
TTjirakit <captain112006@gmail.com>
2024-12-20 12:36:18 +0700

    lab10_2 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,11 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    cout << "Number of data = ";
+    cout << setprecision(3);
+    cout << "Mean = ";
+    cout << "Standard deviation = ";
+}
