964f9a50a2e4f4628a4dc30afe79ca4c62754d85
TTjirakit <captain112006@gmail.com>
2024-12-20 20:39:41 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -3,28 +3,28 @@
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
-double mean(){
-    ifstream source;
-    float num;
-    source.open("score.txt") ;
-    while(true){
-		source >> num;
-        num
-        cout << 
-	}
-    return ;
-}
-
 int main(){
     int count = 0;
+    float sum = 0;
+    float sum_square = 0;
 
-    cout << "Number of data = ";
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+        sum += atof(textline.c_str());
+        sum_square += pow(atof(textline.c_str()),2);
+        count++;
+    }
+
+    cout << "Number of data = " << count << endl;
     cout << setprecision(3);
-    cout << "Mean = ";
-    cout << "Standard deviation = ";
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << sqrt((sum_square/count)-pow(sum/count,2));
 
     return 0;
 }
