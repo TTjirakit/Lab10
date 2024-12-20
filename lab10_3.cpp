b84f82c31c6e57919f6e232e2653639512ad480d
TTjirakit <captain112006@gmail.com>
2024-12-20 15:03:22 +0700

    edit 1

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+double mean(){
+    ifstream source;
+    float num;
+    source.open("score.txt") ;
+    while(true){
+		source >> num;
+        num
+        cout << 
+	}
+    return ;
+}
+
 int main(){
+    int count = 0;
+
     cout << "Number of data = ";
     cout << setprecision(3);
     cout << "Mean = ";
     cout << "Standard deviation = ";
+
+    return 0;
 }
